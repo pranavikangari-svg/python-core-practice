c949736f4624eafcd7cc528ab1a8e27ac4a6daf1
pranavikangari <pranavikangari@gmail.com>
2026-07-22 20:31:09 +0530

    Add files via upload

---
+++ day01_variables_datatypes.py/day1.py
@@ -0,0 +1,51 @@
+# -----------------------------
+# Program 1 - Store Name
+# -----------------------------
+
+name = "Aerin"
+print(name)
+
+
+# -----------------------------
+# Program 2 - Store Age
+# -----------------------------
+
+age = 20
+print(age)
+
+
+# -----------------------------
+# Program 3 - Addition
+# -----------------------------
+
+a = 10
+b = 20
+
+print(a + b)
+
+
+# -----------------------------
+# Program 4 - Swap Numbers
+# -----------------------------
+
+a = 5
+b = 10
+
+a, b = b, a
+
+print(a, b)
+
+
+# -----------------------------
+# Program 5 - Data Types
+# -----------------------------
+
+name = "Aerin"
+age = 20
+height = 5.8
+student = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(student))
